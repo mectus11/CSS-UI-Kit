03e6fbedf0e357b666e018564cb9a2022a50e5a7
mectus11 <salpfadi@gmail.com>
2015-02-28 16:23:19 +0100

    Adding files
    Adding files

---
+++ index.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>CSS Kit</title>
+        <link href="css/essentials.css" rel="stylesheet">
+		<link href="css/menu.css" rel="stylesheet">
+		<link href="css/buttons.css" rel="stylesheet">
+		<link href="css/columns.css" rel="stylesheet">
+        <script src="js/script.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
+    </head>
+	
+	            <body>
+				<p> Menu </p>
+				
+				    <div id='cssmenu'>
+					<ul>
+					   <li><a href='#'>Home</a></li>
+					   <li><a href='#'>Dropdown +</a>
+					      <ul>
+					         <li><a href='#'>Subsection &nbsp; +</a>
+					            <ul>
+					               <li><a href='#'>Stuff</a></li>
+					               <li><a href='#'>Stuff</a></li>
+					            </ul>
+					         </li>
+					         <li><a href='#' class="sub-2">Subsection &nbsp; +</a>
+					            <ul>
+					               <li><a href='#'>Stuff</a></li>
+					               <li><a href='#'>Stuff</a></li>
+					            </ul>
+					         </li>
+					      </ul>
+					   </li>
+					   <li><a href='#'>About</a></li>
+					   <li><a href='#'>Contact</a></li>
+					</ul>
+					</div>
+                    <hr>
+				<div class="grid-container outline">
+				<div class="row">
+					    <div class="container">
+						    <p> Normal Buttons </p>
+							<a class="blankbutton" href="#">Blank Button</a>
+						    <a class="normalbutton" href="#">Normal Button</a>
+						    <a class="warningbutton" href="#">Warning Button</a>
+						    <a class="dangerbutton" href="#">Danger Button</a>
+						    <a class="infobutton" href="#">Info Button</a>
+						    <a class="successbutton" href="#">Success Button</a>
+						    <p> Network Buttons </p>
+						    <a class="facebookbutton" href="#">Check out our page</a>
+						    <a class="twitterbutton" href="#">Tweet something</a>
+						    <a class="githubbutton" href="#">Check it out on Github</a>
+						    <a class="pastebinbutton" href="#">Look at this paste</a>
+							<a class="instagrambutton" href="#">Instagram? Why Not?</a>
+					    </div>
+					
+					    <div class="container">
+						    <a class="youtubebutton" href="#">Check out this channel</a>
+						    <a class="dropboxbutton" href="#">Download it from Dropbox</a>
+						    <a class="googleplusbutton" href="#">Google Plus Page</a>
+							<a class="downloadbutton" href="#">Download File</a>
+							<a class="kickstarterbutton" href="#">Kickstarter Page</a>								
+					    </div>
+						
+					    <div class="container">
+						    <p> Small Buttons </p>
+							<a class="smallfacebookbutton" href="#"></a>
+						    <a class="smalltwitterbutton" href="#"> </a>
+						    <a class="smallgithubbutton" href="#"></a>
+						    <a class="smallpastebinbutton" href="#"></a>
+						    <a class="smallinstagrambutton" href="#"></a>
+							<a class="smallgoogleplusbutton" href="#"></a>
+						    <a class="smallyoutubebutton" href="#"></a>
+						    <a class="smalldropboxbutton" href="#"></a>
+                            <a class="smallkickstarterbutton" href="#"></a>								
+							
+						</div>
+				</div>
+				
+				<div class="row">
+					    <div class="container">
+						    <p> Progressbars </p>
+							<div class="progress-box"> 
+						    <progress max="100" value="30"></progress>
+							</div>
+					    </div>
+				</div>
+				
+				<div class="row">
+				<div class="container">
+					<p> Panels </p>
+					<div class="col-2"> 
+					        <div class="panel-1">
+					        <div class="paneltitle-1">
+                            <center>
+						    <h3> Title </h3>
+						    </center>
+					        </div>
+						    <p>Non alterum finitas tot diversi. 
+						    Gi durent ad se pendam cupiam ut. <br>
+						    Ego affirmans oblivisci via exponetur vim age existeret. <br>
+						    At cumque de semper pulses ut. <br>
+						    Reducantur detorqueat via potentiali supponatur indefinite sic repugnaret per. <br> 
+						    Ut de tritam gi veluti amisit illico.</p>
+						    </div>
+					</div>
+
+					<div class="col-2">
+						    <div class="panel-2">
+					        <div class="paneltitle-2">
+                            <center>
+						    <h3> Title </h3>
+						    </center>
+					        </div>
+						    <p>Non alterum finitas tot diversi. 
+						    Gi durent ad se pendam cupiam ut. <br>
+						    Ego affirmans oblivisci via exponetur vim age existeret. <br>
+						    At cumque de semper pulses ut. <br>
+						    Reducantur detorqueat via potentiali supponatur indefinite sic repugnaret per. <br> 
+						    Ut de tritam gi veluti amisit illico.</p>
+                            </div>		
+					</div>
+
+					<div class="col-1">
+						    <div class="panel-3">
+					        <div class="paneltitle-3">
+                            <center>
+						    <h3> Title </h3>
+						    </center>
+					        </div>
+						    <p>Non alterum finitas tot diversi. 
+						    Gi durent ad se pendam cupiam ut.</p>
+                            </div>		
+					</div>
+                </div>					
+ 				</div>
+				
+				</div>	
+
+
+					
+					</body>
+							
+</html>							
